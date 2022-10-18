6628f525477c3081e265042b1af5442dff4575e2
于瑞 <1738067441@qq.com>
2022-10-18 09:54:43 +0800

    版本1.0

---
+++ src/demo1/user.java
@@ -0,0 +1,19 @@
+package demo1;
+
+public class user {
+    private int id;
+    private String uname;
+
+    public user(int id, String uname) {
+        this.id = id;
+        this.uname = uname;
+    }
+
+    public user() {
+    }
+
+
+    public void show(){
+
+    }
+}
